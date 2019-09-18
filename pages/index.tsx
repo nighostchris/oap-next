2803a75bb2599c34c63e1b3e23f401d6fc7618f8
nighostchris <chrisliupascal@gmail.com>
2019-09-18 15:53:26 +0800

    Setup _app and _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,20 @@
 import * as React from 'react'
 import { NextPage } from 'next'
-import { styletron, debug } from '../styletron'
-import { Provider as StyletronProvider } from 'styletron-react'
-import { BaseProvider, LightTheme, styled } from 'baseui'
+// import { styled } from 'baseui'
 import { StatefulInput } from 'baseui/input'
 
+/*
 const Centered = styled('div', {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     height: '100%',
 });
+*/
 
 const IndexPage: NextPage = () => {
   return (
-    <StyletronProvider value={ styletron } debug={ debug } debugAfterHydration>
-        <BaseProvider theme={ LightTheme }>
-            <Centered>
-                <StatefulInput />
-            </Centered>
-        </BaseProvider>
-    </StyletronProvider>
+    <StatefulInput />
   )
 }
 
