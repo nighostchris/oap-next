7f1420cdd347686ddbce4df4ced7c2399bfb02ba
nighostchris <chrisliupascal@gmail.com>
2019-10-16 15:39:59 +0800

    Assignment dashboard layout review

--- components/course/AssignmentTab.tsx
+++ components/course/AssignmentTab.tsx
@@ -1,34 +1,23 @@
 import * as React from 'react'
 import { styled } from 'baseui'
-import { Button } from 'baseui/button'
 import { useRouter } from 'next/router'
+import printDate from '../../utils/helper'
 import { Label1, Label3, Label4 } from 'baseui/typography'
 import { Assignment } from 'styled-icons/material/Assignment'
 import { AssignmentLate } from 'styled-icons/material/AssignmentLate'
 import { AssignmentTurnedIn } from 'styled-icons/material/AssignmentTurnedIn'
 import Link from 'next/link'
 
-const SubmitButton = styled(Button, {
-  width: '150px',
-  marginLeft: 'auto !important',
-  marginRight: '40px !important',
-  fontWeight: "bolder",
-  fontSize: '14px !important',
-  padding: '5px !important',
-  borderRadius: '5px !important',
-});
-
 const Container = styled('div', {
   width: '90%',
   display: 'flex',
   padding: '20px 0',
-  overflowY: 'scroll',
+  overflowY: 'auto',
   alignItems: 'center',
   flexDirection: 'column',
   backgroundColor: '#90a4ae',
   border: '1px solid #607d8b',
   height: 'calc(100% - 118px)',
-  //borderTop: 'unset',
   borderTopRightRadius: '5px !important',  
   borderBottomLeftRadius: '5px !important',
   borderBottomRightRadius: '5px !important',
@@ -46,15 +35,13 @@ const Container = styled('div', {
 
 const SubContainer = styled('div', {
   width: '90%',
+  borderRadius: '10px',
   marginBottom: '20px',
-  backgroundColor: '#e0e0e0',
-  border: '1px solid #9e9e9e',
 });
 
 const AssignmentTag = styled(Label1, {
   width: '90%',
-  marginBottom: '5px',
-  fontWeight: "bolder",
+  marginBottom: '10px',
 });
 
 const CourseTitle = styled(AssignmentTag, {
@@ -64,55 +51,99 @@ const CourseTitle = styled(AssignmentTag, {
 
 const AssignmentContainer = styled('div', {
   display: 'flex',
-  paddingLeft: '20px',
+  borderRadius: '10px',
   flexDirection: 'row',
   alignItems: 'center',
+  backgroundColor: '#e0e0e0',
   ':hover': {
     backgroundColor: '#bdbdbd',
   },
+  ':not(:last-child)': {
+    marginBottom: '10px',
+  },
 });
 
+const StyledATI = styled(AssignmentTurnedIn, {
+  width: '15%',
+  textAlign: 'center',
+})
+
+const StyledAL = styled(AssignmentLate, {
+  width: '15%',
+  textAlign: 'center',
+})
+
+const StyledA = styled(Assignment, {
+  width: '15%',
+  textAlign: 'center',
+})
+
 const AssignmentDetails = styled('div', {
+  width: '25%',
   display: 'flex',
+  margin: '10px 0',
+  paddingLeft: '30px',
   flexDirection: 'column',
-  margin: '10px 0 10px 20px',
+  alignItems: 'flex-start',
+});
+
+const SubChance = styled(Label3, {
+  width: '50%',
+  textAlign: 'center',
+});
+
+const SButton = styled('button', {
+  width: '10%',
+  padding: '8px',
+  border: 'unset',
+  fontSize: '14px',
+  minWidth: '150px',
+  marginLeft: 'auto',
+  marginRight: '40px',
+  borderRadius: '5px',
+  fontWeight: "bolder",
 });
 
 const Title = styled(Label3, {
-  fontWeight: "bolder",
   marginBottom: '5px',
   ':hover': {
     cursor: 'pointer',
   },
 });
 
+const Breakline = styled('div', {
+  width: '100%',
+  marginBottom: '20px',
+  borderBottom: '1px dashed white',
+});
+
 const assignments = [{
-  code: '2012',
-  number: '1',
+  id: '1',
   title: 'Tic Tac Toe',
   submitted: true,
   dueDate: new Date(2019, 8, 15, 11, 59, 59),
+  chance: 2,
 },
 {
-  code: '2012',
-  number: '4',
+  id: '4',
   title: 'Tic Tac Toe 2',
   submitted: true,
   dueDate: new Date(2019, 8, 15, 11, 59, 59),
+  chance: 1,
 },
 {
-  code: '2012',
-  number: '2',
+  id: '2',
   title: 'Bank System',
   submitted: false,
   dueDate: new Date(2019, 9, 20, 23, 59, 59),
+  chance: 3,
 },
 {
-  code: '2012',
-  number: '3',
+  id: '3',
   title: 'Tic Tac Toe 3',
   submitted: false,
   dueDate: new Date(2019, 8, 16, 11, 59, 59),
+  chance: 3,
 }]
 
 const AssignmentTab: React.FunctionComponent = () => {
@@ -122,102 +153,53 @@ const AssignmentTab: React.FunctionComponent = () => {
   return (
     <Container>
       <CourseTitle>{String(courseid).toUpperCase()}</CourseTitle>
-      <AssignmentTag>Upcoming</AssignmentTag>
-      <SubContainer>
-        {
-          assignments.filter(a => a.dueDate > new Date() && !a.submitted).map((d, index) => (
-            <AssignmentContainer key={`upcoming-${index}`}>
-              <AssignmentTurnedIn size={26} />
-              <AssignmentDetails>
-                <Title>{d.title}</Title>
-                <Label4>
-                  {`
-                    Due Date:
-                    ${d.dueDate.getFullYear()}-${d.dueDate.getMonth() + 1}-${d.dueDate.getDate()}
-                  `}
-                </Label4>
-              </AssignmentDetails>
-              <Link href={`/assignment/${d.number}`}>
-                <SubmitButton
-                  overrides={{
-                    BaseButton: {
-                      style: {
-                        backgroundColor: '#4caf50',
-                      }
-                    }
-                  }}
-                >
-                  Submit
-                </SubmitButton>
-              </Link>
-            </AssignmentContainer>
-          ))
-        }
-      </SubContainer>
-      <AssignmentTag>Submitted</AssignmentTag>
-      <SubContainer>
-        {
-          assignments.filter(a => a.dueDate < new Date() && a.submitted).map((d, index) => (
-            <AssignmentContainer key={`submitted-${index}`}>
-              <AssignmentLate size={26} />
-              <AssignmentDetails>
-                <Title>{d.title}</Title>
-                <Label4>
-                  {`
-                    Due Date:
-                    ${d.dueDate.getFullYear()}-${d.dueDate.getMonth() + 1}-${d.dueDate.getDate()}
-                  `}
-                </Label4>
-              </AssignmentDetails>
-              <Link href={`/assignment/${d.number}`}>
-                <SubmitButton
-                  overrides={{
-                    BaseButton: {
-                      style: {
-                        backgroundColor: '#2196f3',
-                      }
-                    }
-                  }}
-                >
-                  View Submission
-                </SubmitButton>
-              </Link>
-            </AssignmentContainer>
-          ))
-        }
-      </SubContainer>
-      <AssignmentTag>Late</AssignmentTag>
-      <SubContainer>
-        {
-          assignments.filter(a => a.dueDate < new Date() && !a.submitted).map((d, index) => (
-            <AssignmentContainer key={`late-${index}`}>
-              <Assignment size={26} />
-              <AssignmentDetails>
-                <Title color="red">{d.title}</Title>
-                <Label4>
-                  {`
-                    Due Date:
-                    ${d.dueDate.getFullYear()}-${d.dueDate.getMonth() + 1}-${d.dueDate.getDate()}
-                  `}
-                </Label4>
-              </AssignmentDetails>
-              <Link href={`/assignment/${d.number}`}>
-                <SubmitButton
-                  overrides={{
-                    BaseButton: {
-                      style: {
-                        backgroundColor: '#d32f2f',
-                      }
-                    }
-                  }}
-                >
-                  View Submission
-                </SubmitButton>
-              </Link>
-            </AssignmentContainer>
-          ))
-        }
-      </SubContainer>
+      {
+        ['Upcoming', 'Submitted', 'Late'].map((t, i) => (
+          <React.Fragment>
+            <AssignmentTag>{t}</AssignmentTag>
+            <SubContainer>
+              {
+                assignments.filter(i == 0 ? a => a.dueDate > new Date() && !a.submitted
+                  : (i == 1 ? a => a.dueDate < new Date() && a.submitted
+                  : a => a.dueDate < new Date() && !a.submitted)).map((d, index) => 
+                  <AssignmentContainer key={`${t}-${index}`}>
+                    {i == 0 && <StyledATI size={26} />}
+                    {i == 1 && <StyledAL size={26} />}
+                    {i == 2 && <StyledA size={26} />}
+                    <AssignmentDetails>
+                      <Title
+                        overrides={{
+                          Block: {
+                            style: {
+                              fontWeight: 'bold',
+                            }
+                          }
+                        }}
+                      >
+                        {d.title}
+                      </Title>
+                      <Label4>
+                        {`Due Date: ${printDate(d.dueDate)}`}
+                      </Label4>
+                    </AssignmentDetails>
+                    <SubChance>{`Remaining submission chance: ${d.chance}`}</SubChance>
+                    <Link href={`/assignment/${d.id}`}>
+                      <SButton style={{ backgroundColor: i == 0 ? '#4caf50' 
+                        : (i == 1 ? '#2196f3' : '#d32f2f')
+                      }}>
+                        {i == 0 && 'Submit'}
+                        {i == 1 && 'View Submission'}
+                        {i == 2 && 'Submit'}
+                      </SButton>
+                    </Link>
+                  </AssignmentContainer>
+                )
+              }
+            </SubContainer>
+            { i !== 2 && <Breakline /> }
+          </React.Fragment>
+        ))
+      }
     </Container>
   )
 }
---
+++ utils/helper.tsx
@@ -0,0 +1,4 @@
+export default function printDate(date : Date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}
+     ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
