772c9c4ce419161ac3cf6fca7f268a8cbe2167d8
nighostchris <chrisliupascal@gmail.com>
2019-12-04 16:54:06 +0800

    Fix template.tsx error for redeployment

--- components/Template.tsx
+++ components/Template.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { styled } from 'baseui';
+//import { styled } from 'baseui';
 
 const Popup: React.FunctionComponent = () => {
   return (
+    <div>template</div>
   );
 };
 
