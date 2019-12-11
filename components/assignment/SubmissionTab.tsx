836db9ade77a21b0b0eb580bc4af03dd86f2f920
Chris <chrisliupascal@gmail.com>
2019-12-11 21:05:33 +0800

    Options available for table component

--- components/assignment/SubmissionTab.tsx
+++ components/assignment/SubmissionTab.tsx
@@ -18,13 +18,28 @@ const thead = ['Filename', 'Size', 'Time', 'Status'];
 const tbody = [
   <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
   <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
+  <><td><i className="fas fa-clock" />assignment1.zip</td><td>273KB</td><td>Dec 11 2019 10:30:27</td><td><i className="fas fa-clock" /></td></>,
 ];
 
 const AnnouncementTab: React.FunctionComponent = () => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-12 col-xl-9">
-        <Table thead={thead} tbody={tbody} />
+        <Table
+          thead={thead}
+          tbody={tbody}
+          bordered
+          textAlign="center"
+        />
       </div>
       <div className="col-12 col-xl-3">
         <Card
--- components/global/Table.tsx
+++ components/global/Table.tsx
@@ -3,31 +3,57 @@ import React from 'react';
 interface TableProps {
   thead: Array<string>
   tbody: Array<JSX.Element>
+  textAlign?: 'default' | 'center'
+  scrollable?: boolean
+  bordered?: boolean
 }
 
-const Table : React.SFC<TableProps> = ({ thead, tbody }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
+/*
+const scrollbarStyle = {
+  '::-webkit-scrollbar': {
+    width: '.8rem',
+  },
+  '::-webkit-scrollbar-thumb': {
+    borderRadius: '.8rem',
+    backgroundClip: 'padding-box',
+    border: '2px solid transparent',
+    backgroundColor: 'rgba(128, 128, 128, .7)',
+    boxShadow: 'inset -1px -1px 0 rgba(0, 0, 0, .05), inset 1px 1px 0 rgba(0, 0, 0, .05)',
+  },
+};
+*/
+
+
+const Table : React.SFC<TableProps> = ({
+  thead, tbody, textAlign = 'default', scrollable, bordered,
+}) => (
+  <div
+    className={textAlign === 'center' ? 'text-center' : undefined}
+    style={{ ...(scrollable && { 'overflowY': 'auto', 'height': '60vh' }) }}
+  >
+    <table className={`table ${bordered && 'table-bordered'}`}>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          {
+            thead.map((th, index) => (
+              <th key={`th-${index}`} scope="col">{th}</th>
+            ))
+          }
+        </tr>
+      </thead>
+      <tbody>
         {
-          thead.map((th, index) => (
-            <th key={`th-${index}`} scope="col">{th}</th>
+          tbody.map((tr, index) => (
+            <tr>
+              <th scope="row">{index}</th>
+              { tr }
+            </tr>
           ))
         }
-      </tr>
-    </thead>
-    <tbody>
-      {
-        tbody.map((tr, index) => (
-          <tr>
-            <th scope="row">{index}</th>
-            { tr }
-          </tr>
-        ))
-      }
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
 );
 
 export default Table;
