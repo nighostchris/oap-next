763e218fd9ae4f9414289d953c47f7f737f059f5
Chris <chrisliupascal@gmail.com>
2020-02-07 17:34:51 +0800

    Courses dashboard component connect to graphql

--- components/course/CourseDashboard.tsx
+++ components/course/CourseDashboard.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { useRouter } from 'next/router';
+import { useQuery, gql } from '@apollo/client';
 import PageHeader from '../global/PageHeader';
 import CourseworkTab from './CourseworkTab';
 import AnnouncementTab from './AnnouncementTab';
 import GradeTab from './GradeTab';
+import timestampConverter from '../../utilities/timestampConverter';
 
 const listItem = [
   {
@@ -32,17 +34,56 @@ const listItem = [
   },
 ];
 
-const pageHeaderProps = {
-  pretitle: 'Desmond Tsoi - L2',
-  title: 'COMP1021 - Introduction to Computer Science',
-  avatar: <img alt="" src="https://www.cse.ust.hk/admin/people/faculty/photos/desmond.jpg" className="avatar-img rounded-circle border border-4 border-card" />,
-  tabTitle: ['Announcements', 'Assignments', 'Labs', 'Grades'],
-};
+const GET_COURSE_DATA = gql`
+  query getCourseData($code: String!) {
+    courses(where: {code: {_eq: $code}}) {
+      code
+      name
+      assignments {
+        name
+      }
+      created_at
+      sections(where: {students: {user: {itsc: {_eq: "kristopher"}}}}) {
+        name
+      }
+    }
+  }
+`;
 
 const CourseDashboard: React.FunctionComponent = () => {
   const router = useRouter();
   const { courseid, coursetab } = router.query;
   const rootUrl = `/course/${courseid}`;
+  const { loading, error, data } = useQuery(GET_COURSE_DATA, {
+    variables: { code: String(courseid).toUpperCase() }
+  });
+
+  const infoList = [];
+  const pageHeaderProps = {
+    pretitle: '',
+    title: '',
+    avatar: <img alt="" src="" className="avatar-img rounded-circle border border-4 border-card" />,
+    tabTitle: ['Announcements', 'Assignments', 'Labs', 'Grades']
+  };
+
+  if (!loading) {
+    infoList.push({
+      category: 'Assignments',
+      value: String(data.courses[0].assignments.length),
+    }, {
+      category: 'Created',
+      value: timestampConverter(new Date(data.courses[0].created_at), false),
+    });
+
+    pageHeaderProps['pretitle'] = 'Desmond Tsoi';
+    pageHeaderProps['title'] = `${data.courses[0].code} - ${data.courses[0].name}`;
+    pageHeaderProps['avatar'] = <img alt="" src="https://www.cse.ust.hk/admin/people/faculty/photos/desmond.jpg" className="avatar-img rounded-circle border border-4 border-card" />;
+    pageHeaderProps['tabTitle'] = ['Announcements', 'Assignments', 'Labs', 'Grades'];
+  }
+
+  if (error) {
+    console.log(error);
+  }
 
   const activeTab = (tab: string | string[]) => {
     switch (tab) {
@@ -69,7 +110,7 @@ const CourseDashboard: React.FunctionComponent = () => {
         rootUrl={rootUrl}
         active={Number(activeTab(coursetab))}
       />
-      { coursetab === 'announcements' && <AnnouncementTab /> }
+      { coursetab === 'announcements' && <AnnouncementTab infoList={infoList} /> }
       { coursetab === 'assignments' && <CourseworkTab title="Assignments" coursework={listItem} /> }
       { coursetab === 'labs' && <CourseworkTab title="Labs" coursework={listItem} /> }
       { coursetab === 'grades' && <GradeTab />}
